779fc7187938b33c42aa7f2336b0e5dc55f4dd5a
Freddy Montes <freddymontes@gmail.com>
2021-01-03 21:57:42 -0600

    Add system font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,32 @@ module.exports = {
     purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        // container: {
-        //     center: true
-        // },
+        fontFamily: {
+            display: [
+                '-apple-system',
+                'BlinkMacSystemFont',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica',
+                'Arial',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol'
+            ],
+            body: [
+                '-apple-system',
+                'BlinkMacSystemFont',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica',
+                'Arial',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol'
+            ]
+        },
         extend: {
             fontSize: {
                 '4xl': ['3.815rem', '4.5rem'],
