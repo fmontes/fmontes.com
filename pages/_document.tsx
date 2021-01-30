4ab8a08eea1b7f8ddf7f1a43d5eb8f0c1a3a9502
Freddy Montes <freddymontes@gmail.com>
2021-01-30 14:38:40 -0600

    Add icons and site.webmanifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,38 @@ class MyDocument extends Document {
                             />
                         </>
                     ) : null}
+                    <meta content="2c7afbfb9165a295" name="yandex-verification" />
+
+                    <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+                    <link href="/static/favicons/site.webmanifest" rel="manifest" />
+                    <link
+                        href="/static/favicons/apple-touch-icon.png"
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                    />
+                    <link
+                        href="/static/favicons/favicon-32x32.png"
+                        rel="icon"
+                        sizes="32x32"
+                        type="image/png"
+                    />
+                    <link
+                        href="/static/favicons/favicon-16x16.png"
+                        rel="icon"
+                        sizes="16x16"
+                        type="image/png"
+                    />
+                    <link
+                        color="#07313A"
+                        href="/static/favicons/safari-pinned-tab.svg"
+                        rel="mask-icon"
+                    />
+                    <meta content="#ffffff" name="theme-color" />
+                    <meta content="#ffffff" name="msapplication-TileColor" />
+                    <meta
+                        content="/static/favicons/browserconfig.xml"
+                        name="msapplication-config"
+                    />
                 </Head>
                 <body>
                     <Main />
