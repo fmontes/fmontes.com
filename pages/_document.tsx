249f613ee53aaa2263b7dce2f9952618ff1e850d
Freddy Montes <freddymontes@gmail.com>
2021-02-01 12:24:14 -0600

    Fix favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,36 +27,29 @@ class MyDocument extends Document {
                     ) : null}
                     <meta content="2c7afbfb9165a295" name="yandex-verification" />
 
-                    <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-                    <link href="/static/favicons/site.webmanifest" rel="manifest" />
+                    <link href="/static/favicon.ico" rel="shortcut icon" />
+                    <link href="/static/site.webmanifest" rel="manifest" />
                     <link
-                        href="/static/favicons/apple-touch-icon.png"
+                        href="/static/apple-touch-icon.png"
                         rel="apple-touch-icon"
                         sizes="180x180"
                     />
                     <link
-                        href="/static/favicons/favicon-32x32.png"
+                        href="/static/favicon-32x32.png"
                         rel="icon"
                         sizes="32x32"
                         type="image/png"
                     />
                     <link
-                        href="/static/favicons/favicon-16x16.png"
+                        href="/static/favicon-16x16.png"
                         rel="icon"
                         sizes="16x16"
                         type="image/png"
                     />
-                    <link
-                        color="#07313A"
-                        href="/static/favicons/safari-pinned-tab.svg"
-                        rel="mask-icon"
-                    />
+                    <link color="#07313A" href="/static/safari-pinned-tab.svg" rel="mask-icon" />
                     <meta content="#ffffff" name="theme-color" />
                     <meta content="#ffffff" name="msapplication-TileColor" />
-                    <meta
-                        content="/static/favicons/browserconfig.xml"
-                        name="msapplication-config"
-                    />
+                    <meta content="/static/browserconfig.xml" name="msapplication-config" />
                 </Head>
                 <body>
                     <Main />
