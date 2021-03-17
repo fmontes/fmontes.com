fe6b3fc20b6b50916a27b688650b5ddad504cf5c
Freddy Montes <freddymontes@gmail.com>
2021-03-17 05:51:38 -0600

    Rearrange elements in the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,44 +14,46 @@ function Header(): JSX.Element {
     return (
         <>
             <header>
-                <Link href="/">
-                    <a className="inline-flex items-center mb-2 no-underline">
-                        <div className="flex-shrink-0 mr-4">
-                            <Image
-                                alt="Freddy Montes - Frontend Developer, Designer and Teacher"
-                                className="rounded-full"
-                                height={AVATAR_SIZE}
-                                src="/images/avatar.jpg"
-                                width={AVATAR_SIZE}
-                            />
-                        </div>
-                        <h2 className="font-bold tracking-tight m-0 text-xl">Freddy Montes</h2>
-                    </a>
-                </Link>
+                <div className="sm:flex sm:justify-between sm:items-center">
+                    <Link href="/">
+                        <a className="inline-flex items-center mb-2 no-underline">
+                            <div className="flex-shrink-0 mr-4">
+                                <Image
+                                    alt="Freddy Montes - Frontend Developer, Designer and Teacher"
+                                    className="rounded-full"
+                                    height={AVATAR_SIZE}
+                                    src="/images/avatar.jpg"
+                                    width={AVATAR_SIZE}
+                                />
+                            </div>
+                            <h2 className="font-bold tracking-tight m-0 text-xl">Freddy Montes</h2>
+                        </a>
+                    </Link>
+                    <Nav />
+                </div>
+
                 <p className="m-0">{t('bio')}</p>
 
                 <div className="my-3 sm:flex sm:justify-between sm:items-center">
-                    <Nav />
+                    <div className="flex items-center mb-3 sm:mb-0">
+                        <p className="text-blue-900 inline-flex items-center m-0 mr-3 pr-3 pl-1 text-sm bg-blue-100 rounded-full">
+                            <svg
+                                baseProfile="tiny"
+                                className="scale-75 transform"
+                                height="24"
+                                width="24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    className="fill-current"
+                                    d="M10.368 19.102c.35 1.05 1.01 1.086 1.478.086l5.31-11.375c.467-1.002.034-1.434-.967-.967L4.812 12.154c-1 .467-.963 1.13.085 1.48L9 15l1.368 4.102z"
+                                />
+                            </svg>{' '}
+                            Costa Rica (GMT-6)
+                        </p>
+                        <Translate />
+                    </div>
                     <SocialMediaMenu />
-                </div>
-
-                <div className="flex items-center mb-6">
-                    <p className="text-blue-900 inline-flex items-center m-0 mr-3 pr-3 pl-1 text-sm bg-blue-100 rounded-full">
-                        <svg
-                            baseProfile="tiny"
-                            className="scale-75 transform"
-                            height="24"
-                            width="24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                className="fill-current"
-                                d="M10.368 19.102c.35 1.05 1.01 1.086 1.478.086l5.31-11.375c.467-1.002.034-1.434-.967-.967L4.812 12.154c-1 .467-.963 1.13.085 1.48L9 15l1.368 4.102z"
-                            />
-                        </svg>{' '}
-                        Costa Rica (GMT-6)
-                    </p>
-                    <Translate />
                 </div>
             </header>
         </>
