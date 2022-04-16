e79bacc06c61b747ab7afd9e419ac6fd496254bf
Freddy Montes <freddymontes@gmail.com>
2022-04-16 13:21:08 -0600

    Remove the logo from flayyer image

--- fmontes-flayyer/templates/main.tsx
+++ fmontes-flayyer/templates/main.tsx
@@ -4,27 +4,6 @@ import { TemplateProps } from '@flayyer/flayyer-types';
 
 import '../styles/tailwind.css';
 
-import angular from '../../public/images/_logos/angular.svg';
-import css from '../../public/images/_logos/css.svg';
-import dragula from '../../public/images/_logos/dragula.svg';
-import fmontes from '../../public/images/_logos/fmontes.svg';
-import gatsby from '../../public/images/_logos/gatsby.svg';
-import nextjs from '../../public/images/_logos/nextjs.svg';
-import react from '../../public/images/_logos/react.svg';
-import sass from '../../public/images/_logos/sass.svg';
-import stencil from '../../public/images/_logos/stencil.svg';
-
-const logos = {
-    angular,
-    css,
-    dragula,
-    gatsby,
-    nextjs,
-    react,
-    sass,
-    stencil
-};
-
 // import logo from '../static/logo.svg';
 import background from '../static/background.png';
 
@@ -47,13 +26,13 @@ const validator = new Validator(schema);
 
 // Make sure to 'export default' a React component
 export default function MainTemplate(props: TemplateProps<Variables>) {
-    const { width, height, variables } = props;
+    const { variables } = props;
 
     if (!validator.validate(variables)) {
         return <img className="w-full h-full object-cover" src={background} />; // Fallback for invalid variables
     }
 
-    const { title, image, logo } = variables;
+    const { title, image } = variables;
     return (
         <>
             <Layer>
@@ -67,9 +46,6 @@ export default function MainTemplate(props: TemplateProps<Variables>) {
                 }}
             />
             <Layer className="flex flex-col justify-center items-center px-4 py-4 text-white text-center relative">
-                <div className="absolute top-2 right-2 bg-white rounded p-1">
-                    <img src={(logo && logos[logo]) || fmontes} className="h-6" />
-                </div>
                 {title && (
                     <h1
                         className="hidden banner:block text-2xl font-bold tracking-tight leading-tight mt-1"
