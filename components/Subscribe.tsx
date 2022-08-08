10aad4b81823a1a02ec704609edc656e21f58f6f
Freddy Montes <freddymontes@gmail.com>
2022-08-08 00:15:17 -0600

    Add revue

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -5,16 +5,30 @@ function Subscribe() {
             <p className="mb-6">
                 Get emails about web development, tech, and early access to new articles.
             </p>
-            <form className="flex flex-col gap-4">
+
+            <form
+                action="https://www.getrevue.co/profile/fmontes/add_subscriber"
+                className="flex flex-col gap-4"
+                id="revue-form"
+                method="post"
+                name="revue-form"
+                target="_blank"
+            >
                 <input
                     className="border-b-4 border-solid border-cyan-600 block w-full py-2 px-4 bg-cyan-200 text-lg font-bold placeholder:text-cyan-600 mb"
-                    placeholder="email@company.com"
-                    type="text"
+                    id="member_email"
+                    name="member[email]"
+                    placeholder="Your email address..."
+                    type="email"
                 />
                 <div className="flex gap-6 items-center">
-                    <button className="bg-cyan-900 text-white text-md font-bold px-6 py-2 rounded-lg">
-                        Subscribe
-                    </button>
+                    <input
+                        className="bg-cyan-900 text-white text-md font-bold px-6 py-2 rounded-lg"
+                        id="member_submit"
+                        name="member[subscribe]"
+                        type="submit"
+                        value="Subscribe"
+                    />
                     <small className="md:text-base">
                         Swear on the the that no spam will be send!
                     </small>
