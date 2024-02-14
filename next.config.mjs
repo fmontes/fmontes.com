777a7f4a2a42cc78f1d939f78cbd79f82eafe626
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-13 20:29:55 -0600

    Render mdx pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
-export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter]
+  }
+})
+
+export default withMDX(nextConfig);
+// export default nextConfig;
