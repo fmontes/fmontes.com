b5aa1ceaa72771b27d8327f157d90887c053c830
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-14 17:28:00 -0600

    Add type to the params in page

--- src/app/[lang]/[slug]/page.tsx
+++ src/app/[lang]/[slug]/page.tsx
@@ -3,7 +3,11 @@ import matter from 'gray-matter';
 import path from 'path';
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
-export default async function Page({ params }) {
+export default async function Page({ params }: {
+  params: {
+    slug: string, lang: string
+  }
+}) {
   const FOLDER = path.resolve(process.cwd(), 'src/data/pages')
   const fullPath = path.join(FOLDER, `${params.lang}/${params.slug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, 'utf-8');
