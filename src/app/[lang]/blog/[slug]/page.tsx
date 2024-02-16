abae5cad09b9946a9161f5e4ece3da68c26e94cf
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-15 21:16:54 -0600

    Add tailwindcss/typography plugin and update code formatting

--- src/app/[lang]/blog/[slug]/page.tsx
+++ src/app/[lang]/blog/[slug]/page.tsx
@@ -1,14 +1,19 @@
-import { getPostBySlug } from "@/utils/content";
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import Image from 'next/image'
+import { getPostBySlug } from '@/utils/content';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import Image from 'next/image';
 
-export default function Blog({ params }: { params: { slug: string, lang: string } }) {
+export default function Blog({ params }: { params: { slug: string; lang: string } }) {
   const post = getPostBySlug(params.lang, params.slug);
 
-  return <>
-    <h1>{post.title}</h1>
-    <MDXRemote source={post.content} components={{
-      Image: (props) => <Image {...props} />
-    }} />
-  </>;
+  return (
+    <main className="prose lg:prose-xl mx-auto">
+      <h1>{post.title}</h1>
+      <MDXRemote
+        source={post.content}
+        components={{
+          Image: (props) => <Image {...props} />,
+        }}
+      />
+    </main>
+  );
 }
