9e93b11c74ac5e1044b53f177c2f9ee7bf6363a0
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-15 21:06:25 -0600

    Remove unnecessary line in page.tsx

--- src/app/[lang]/blog/[slug]/page.tsx
+++ src/app/[lang]/blog/[slug]/page.tsx
@@ -2,7 +2,6 @@ import { getPostBySlug } from "@/utils/content";
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import Image from 'next/image'
 
-
 export default function Blog({ params }: { params: { slug: string, lang: string } }) {
   const post = getPostBySlug(params.lang, params.slug);
 
