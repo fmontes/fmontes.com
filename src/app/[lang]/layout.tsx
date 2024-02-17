0c50cae523057013f2e822f0b53b3484d1a008cd
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-17 07:22:15 -0600

    Add favicon and manifest files to layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -20,9 +20,32 @@ export default function RootLayout({
   };
   children: React.ReactNode;
 }>) {
-  
+
   return (
     <html lang={params.lang}>
+      <head>
+        <link href="/static/favicon.ico" rel="shortcut icon" />
+        <link href="/static/site.webmanifest" rel="manifest" />
+        <link
+          href="/static/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/static/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/static/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link color="#07313A" href="/static/safari-pinned-tab.svg" rel="mask-icon" />
+        <meta content="/static/browserconfig.xml" name="msapplication-config" />
+      </head>
       <body className={`${inter.className} dark:bg-blue-900`}>
         <div className="container p-6 md:p-8 lg:px-0 antialiased mx-auto">
           <Header />
