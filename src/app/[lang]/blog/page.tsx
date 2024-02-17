0d90e5184cfd1a8885e80282ebfe0d3f71171420
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-16 20:24:57 -0600

    Add /blog

---
+++ src/app/[lang]/blog/page.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+import { getPosts } from '@/utils/content';
+import { Date } from '@/components/Date';
+
+export default function Home({
+  params,
+}: {
+  params: {
+    slug: string;
+    lang: string;
+  };
+}) {
+  if (params.lang === 'sw.js') return null;
+
+  const posts = getPosts(params.lang);
+
+  return (
+    <main className="prose lg:prose-xl mx-auto">
+      <h2 className="text-2xl font-bold sm:text-3xl tracking-tight sm:leading-tight mb-6">
+        {/* {t('latest_blog_posts')} */}
+      </h2>
+      <section className=" gap-6 mb-16">
+        {posts.map((post, i: number) => {
+          return (
+            <div key={i}>
+              <p>
+                <Link href={`/${params.lang}/blog/${post.slug}`}>{post.title}</Link>
+              </p>
+              <p>
+                <Date date={post.date} locale={params.lang} />
+              </p>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
