d8722cb627b6c7cd1135ea61c93e76d6d180871a
fmontes <751424+fmontes@users.noreply.github.com>
2024-12-07 08:40:11 -0600

    refactor: Enhance async parameter handling in Blog component
    Updated the Blog component in page.tsx to await the params promise, ensuring accurate parameter management for data fetching. This change improves the reliability of metadata generation and URL construction for Open Graph data and images, resulting in better overall data handling.

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -5,19 +5,25 @@ import CardItem from '@/components/CardItem';
 import { SITE } from '@/utils/const';
 import { notFound } from 'next/navigation';
 
-export async function generateMetadata({ params }: { params: PageParams }) {
-  const dictionary = await getDictionary(params.lang);
-  const defaultOpenGraph = await getDefaultOpenGraph(params.lang);
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<PageParams>
+}) {
+  const pageParams = await params;
+
+  const dictionary = await getDictionary(pageParams.lang);
+  const defaultOpenGraph = await getDefaultOpenGraph(pageParams.lang);
 
   return {
     title: 'Freddy Montes - Blog',
     openGraph: {
       ...defaultOpenGraph,
       title: 'Freddy Montes - Blog',
-      url: `${SITE}/${params.lang}/${params.slug}`,
+      url: `${SITE}/${pageParams.lang}/blog`,
       images: [
         {
-          url: `${SITE}/static/images/banner_${params.lang}.png`,
+          url: `${SITE}/static/images/banner_${pageParams.lang}.png`,
           alt: `${dictionary.title} - ${dictionary.description}`,
           width: 1200,
           height: 630,
@@ -27,12 +33,17 @@ export async function generateMetadata({ params }: { params: PageParams }) {
   };
 }
 
-export default async function Home({ params }: { params: PageParams }) {
+export default async function Blog({
+  params,
+}: {
+  params: Promise<PageParams>
+}) {
+  const pageParams = await params;
 
-  if ((params.lang as any) === 'sw.js') return null;
+  if ((pageParams.lang as any) === 'sw.js') return null;
 
-  const dictionary = await getDictionary(params.lang);
-  const posts = getPosts(params.lang);
+  const dictionary = await getDictionary(pageParams.lang);
+  const posts = getPosts(pageParams.lang);
 
   return (
     <main className="max-w-4xl mx-auto mt-12 prose dark:prose-invert lg:prose-md xl:prose-lg prose-h2:my-0 lg:prose-h2:my-0 xl:prose-h2:my-0 prose-h2:mb-4 lg:prose-h2:mb-4 xl:prose-h2:mb-4 dark:prose-a:text-white prose-a:text-blue-900 dark:prose-h2:text-yellow">
